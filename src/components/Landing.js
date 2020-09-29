6a9bff1fe6b1151ce1c71f529cafc21848f03065
josegocampo <jose.ocampo@mail.udp.cl>
2020-09-28 23:08:28 -0300

    moving foward with the styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -64,59 +64,26 @@ const Landing = () =>{
 export default Landing;
 
 const Main = S.div` 
-    
-    height: 450px;
-    width: 373px;
-    margin-top: 20px;
-  
-
-    
-
+  height: 450px;
+  width: 373px;
   `
-  // const breatheAnimation = keyframes`
-  // 0% {    backdrop-filter: blur(8px);
-  //   background: rgba(255, 255, 255, 0.2);   }
-  // 10% {    backdrop-filter: blur(7.2px);
-  //   background: rgba(255, 255, 255, 0.18);   }
-  // 20% {   backdrop-filter: blur(6.4px); 
-  //   background: rgba(255, 255, 255, 0.16);  }
-  // 30% {    backdrop-filter: blur(5.6px);
-  //   background: rgba(255, 255, 255, 0.14);  }
-  // 40% {   backdrop-filter: blur(4.8px); 
-  //     background: rgba(255, 255, 255, 0.12);  }
-  // 50% {    backdrop-filter: blur(4px);
-  //     background: rgba(255, 255, 255, 0.1);  }
-  // 60% {    backdrop-filter: blur(3.2px);
-  //       background: rgba(255, 255, 255, 0.08);  }
-  // 70% {   backdrop-filter: blur(2.4px); 
-  //         background: rgba(255, 255, 255, 0.06);  }
-  // 80% {    backdrop-filter: blur(1.6px);
-  //         background: rgba(255, 255, 255, 0.04);  }    
-  // 90% {   backdrop-filter: blur(0.8px); 
-  //           background: rgba(255, 255, 255, 0.02);  }
-  // 100% {    backdrop-filter: blur(0px);
-  //           background: rgba(255, 255, 255, 0);  }    
-  
- 
-
 const Content = S.div` 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100%;
-    justify-content: space-between;
-    background-image: url(${cancha});
-    background-size: 373px 450px;
-    background-position: 0% 100%;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  justify-content: space-between;
+  background-image: url(${cancha});
+  background-size: 373px 450px;
+  background-position: 0% 100%;
   `
-
 const Title = S.h1`
   font-size: 35px;
   font-weight: 400;
   width: 100%; 
   height: 100px;
-  margin-bottom: 0;
+  margin-bottom: 0px;
+  margin-top: 0px;
   color: black;
   display: flex;
   flex-direction: row;
@@ -129,29 +96,16 @@ const Title = S.h1`
   text-align: center;
   z-index: 100;
   background-image: linear-gradient(transparent 50%,#f6f5f0 50%);
-  
-
-
 `
 
-// background: #edece3;
-// background-position: 0 100%;
-// background-size: 100% 2px;
-
-// text-align: center;
-// z-index: 100;
-// background-image: linear-gradient(transparent 50%,#f6f5f0 50%);
-
 const Buttons = S.div`
-    height: 150px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    text-decoration: none;
-    margin-top: 200px;
-  
-
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  margin-top: 200px;
 `
 
 
--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,105 +1,167 @@
 import axios from "axios";
 import React, { useEffect, useState, useContext } from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import PlayersDataContext from './PlayersDataContext';
 import S from 'styled-components';
+import esperanza from '../images/esperanza.png'
 
-const NewGame = () =>{
 
-    
-    const [ playersData, setPlayersData ] = useState()
+const NewGame = () => {
+
+    const [playersData, setPlayersData] = useState()
 
     const { selectedPlayers, setSelectedPlayers } = useContext(PlayersDataContext)
 
     useEffect(() => {
         axios.get('https://hcesperanzino.herokuapp.com/players')
-        .then(response =>  {
-            setPlayersData(response.data)
+            .then(response => {
+                setPlayersData(response.data)
             })
-        .catch(error => {console.log(error)
+            .catch(error => {
+                console.log(error)
             })
-        }, 
+    },
         [])
 
     //this function will take care of removing the selected players from the dropdown
     //and add them to the display
     const handleSelect = (e) => {
-        const copyPd = [...playersData]
-        let removed = undefined
-        copyPd.map((p, i) => {
-            if (p.id == e.target.value){
-              removed = copyPd.splice(i,1) 
+        if (e.target.value === "nope") {
+            return null
+        }
+        else {
+            let copied = undefined
+            playersData.map((p, i) => {
+                if (p.id == e.target.value) {
+                    copied = p
+                }
+            })
+            if (!selectedPlayers.length){
+                setSelectedPlayers([...selectedPlayers, copied])
             }
-        })
-        setPlayersData(copyPd)
-        setSelectedPlayers([...selectedPlayers, removed[0]])
+            else{
+                let itsThere = false
+                selectedPlayers.forEach(( player ) => {
+                    if ( player.id === copied.id){
+                        itsThere = true
+                    }
+                })
+                if ( !itsThere ){
+                    setSelectedPlayers([...selectedPlayers, copied])
+                }
+            }
+      
+        }
     }
     //this function will take care of removing players from the display and adding them
     //back to the dropdown
-    const handleChange = (player) =>{
+    const handleDelete = (player) => {
         const copySelectedPlayers = [...selectedPlayers]
-        let removed = undefined
-        copySelectedPlayers.map((p,i) =>{
-            if (p.id == player.id){
-                removed = copySelectedPlayers.splice(i,1) 
-              }
+        copySelectedPlayers.map((p, i) => {
+            if (p.id == player.id) {
+                copySelectedPlayers.splice(i, 1)
+            }
         })
         setSelectedPlayers(copySelectedPlayers)
-        setPlayersData([...playersData, removed[0]])
     }
 
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault()
     }
-    
+
 
     console.log(playersData)
-  
+
     return (
         <Card onSubmit={handleSubmit}>
-            <h2 className="hole">SELECT THE PLAYERS</h2>
-            <Select name="players" onChange={handleSelect} className="title">
-                {playersData ? playersData.map((p) => <option value={p.id} id={p.name} className="title">{p.name}</option>) : <h2>Waiting for Players Data</h2>}
-            </Select>
-            {selectedPlayers ? selectedPlayers.map((p) => <PDisplay className="title">{p.name} <button style={{width:30}} onClick={() => {handleChange(p)}}>X</button></PDisplay>) : null}
-            {selectedPlayers && selectedPlayers.length >=2 && selectedPlayers.length < 5  ? 
-            <Link to ="/game" tyle={{ textDecoration: 'none' }}><Continue className="continue">Continue to Game</Continue></Link> : <Warning className="hole">Please select 2 to 4 players to proceed!</Warning>}
+            <Title><img src={esperanza} className="oak" /></Title>
+            <h3 className="title">SELECT THE PLAYERS</h3>
+            <DropDown>
+                {selectedPlayers.length >= 4 ? null :
+                    <Select name="players" onChange={handleSelect} className="card" autofocus="disabled">
+                        <option value="nope" selected>--</option>
+                        {playersData ? playersData.map((p) =>
+                            <option value={p.id} id={p.name} className="card">{p.name}</option>) :
+                            <h2>Waiting for Players Data</h2>}
+                    </Select>}
+            </DropDown>
+            <Selected>
+                {selectedPlayers.map((p) => <PDisplay className="card">{p.name} <button style={{ width: 30 }}
+                    onClick={() => { handleDelete(p) }}> X </button></PDisplay>)}
+            </Selected>
+            <Bottom>
+                {selectedPlayers && selectedPlayers.length >= 2 && selectedPlayers.length < 5 ?
+                    <Link to="/game" tyle={{ textDecoration: 'none' }}><Continue className="continue">Continue to Game</Continue></Link>
+                    : <Warning className="hole">Please select 2 to 4 players to proceed!</Warning>}
 
+            </Bottom>
         </Card>
     )
 }
 
 const Card = S.form`
-    margin-top: 20px;   
     width: 373px;
-    height: 450px;
     display: flex;
     flex-direction: column;
     align-items: center;
     background: #edece3;
+    height: 550px;
+`
+const Title = S.h1`
+    font-size: 35px;
+    font-weight: 400;
+    width: 100%; 
+    height: 100px;
+    margin-top: 0;
+    color: black;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background: #edece3;
+    background-position: 0 100%;
+    background-size: 100% 2px;
+    border-bottom: 3px solid #762417c4;
+    card-align: center;
+    z-index: 100;
+    background-image: linear-gradient(transparent 50%,#f6f5f0 50%);
+`
 
+const DropDown = S.div`
+    width: 200px;
+    height: 20px;
 `
 const Select = S.select`
-    width: 200px;
+    width: 150px;
 `
+const Selected = S.div`
+    height: 200px;
+
+`
+
 const PDisplay = S.div`
-    margin-top: 20px;
+    margin-top: 10px;
+    width: 200px;
+    text-align: center;
 `
+
+const Bottom = S.div`
+    margin-bottom: 20px;
+`
+
 const Continue = S.button`
     width: 150px;
     height: 50px;
     border-radius: 5px;
     border: none;
-    background: #7aa9e5;
+    background: rgb(48 134 48);
     font-weight: 500;
-    margin-top: 30px;
-` 
+    color: white;
+`
 
 const Warning = S.div`
-    margin-top: 80px;
-    width: 150px;
-    height: 80px;
-    color: #762417c4;
+    font-weight: 600;
+    font-size: 1.1rem;
 `
 
 export default NewGame;
