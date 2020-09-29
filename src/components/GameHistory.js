168cb6d74861985a1fd7ccdd09d259418a7ed775
josegocampo <jose.ocampo@mail.udp.cl>
2020-09-29 16:40:34 -0300

    game flow ready and game history ready

--- src/components/GameHistory.js
+++ src/components/GameHistory.js
@@ -1,41 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import S from 'styled-components';
+import GameHistoryDisplay from './GameHistoryDisplay'
 
 
 const GameHistory = () => {
 
-    const [ gameHistory, setGameHistory ] = useState()
+    const [gameHistory, setGameHistory] = useState()
 
-    const [ gameDisplay, setGameDisplay ] = useState()
 
     useEffect(() => {
-         axios.get(`https://hcesperanzino.herokuapp.com/games/gameinfo`)
-        .then((response) => { 
-            setGameHistory(response.data
-                )})
-        .catch((err) => {
-            console.log(err)
-        })
-    },[])
+        axios.get(`https://hcesperanzino.herokuapp.com/games/gameinfo`)
+            .then((response) => {
+                setGameHistory(response.data)
+                console.log('hi')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
-    
+
+
 
     return (
-        <div>
-            {/* <Card>
-                {gameHistory.map((game) => 
-                <div>
-                    <span>{game.game_id}</span><span>{game_game}</span><span></span><span></span>
-                </div>
-                )}
-            </Card> */}
-
-        </div>
+                    <div><GameHistoryDisplay data={gameHistory}/></div>
     )
 
-
 }
+
+
 
 
 export default GameHistory;
